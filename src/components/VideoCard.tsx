3f27716f7f7f1eccec0395557e1acbb20351664e
Chinmay <chinmaybagad4@gmail.com>
2024-06-22 00:25:48 +0530

    Added VideoCard Component that loads individual card when entered correct props

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,23 @@
+const VideoCard = (props: any) => {
+  return (
+    <div>
+      <img src={props.image} className=" rounded-xl" />
+      <div className="grid grid-cols-12">
+        <div className=" col-span-1 mt-2">
+          <img src={props.thumb} className="rounded-full h-10 w-10  ml-4" />
+        </div>
+        <div className="col-span-11 pl-8 xl:pl-0">
+          <div>{props.title}</div>
+          <div className="col-span-11 text-base text-gray-500">
+            {props.author}
+          </div>
+          <div className="col-span-11 text-base text-gray-500">
+            {props.view} | {props.timeStamp}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
